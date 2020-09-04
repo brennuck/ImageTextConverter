958d720715042b9c848d5c8acf92f3065beb60f6
Brennon Nuckols <brennuck@gmail.com>
2020-09-04 09:26:17 -0600

    ImageUploader rendering

--- image_txt/src/App.js
+++ image_txt/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import ImageUploader from 'react-images-upload';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            pictures: []
+        }
+        this.onDrop = this.onDrop.bind(this);
+    }
+
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <ImageUploader
+                    withIcon={true}
+                    buttonText='Choose images'
+                    onChange={this.onDrop}
+                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                    maxFileSize={5242880}
+                />
+            </div>
+        )
+    }
 }
 
 export default App;
