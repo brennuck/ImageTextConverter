4b5fc52a95663e36c4f519b6a9f616076dc819cb
Brennon Nuckols <brennuck@gmail.com>
2020-09-10 09:38:37 -0600

    no more lorum

--- image_txt/src/App.js
+++ image_txt/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
         super();
         this.state = {
             pictures: "",
-            text: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?",
+            text: "",
             loading: false,
             copied: false,
         }
